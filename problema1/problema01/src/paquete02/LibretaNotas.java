54e4493ff39915410f74f7077fd4b64e3762b21b
unknown <estoledo1@utpl.edu.ec>
2024-05-08 16:18:16 -0500

    problema01

---
+++ problema1/problema01/src/paquete02/LibretaNotas.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paquete02;
+
+/**
+ *
+ * @author utpl
+ */
+public class LibretaNotas {
+
+    private String nombreEstudiante;
+    private double calificacion1;
+    private double calificacion2;
+    private double calificacion3;
+    private double promedio;
+
+//
+    public LibretaNotas(String n, double c1, double c2, double c3) {
+        nombreEstudiante = n;
+        calificacion1 = c1;
+        calificacion2 = c2;
+        calificacion3 = c3;
+    }
+
+    public LibretaNotas(String n, double c1, double c2) {
+        nombreEstudiante = n;
+        calificacion1 = c1;
+        calificacion2 = c2;
+        calificacion3 = 10;
+    }
+
+    public void estableverNombreEstudiante(String e) {
+
+        nombreEstudiante = e;
+    }
+
+    public void establecerCalificacion1(double c1) {
+        calificacion1 = c1;
+    }
+
+    public void establecerCalificacion2(double c2) {
+        calificacion2 = c2;
+    }
+
+    public void establecerCalificacion3(double c3) {
+        calificacion3 = c3;
+    }
+
+    public void establecerPromedio() {
+        promedio = (calificacion1 + calificacion2 + calificacion3) / 3;
+    }
+
+    public double obtenerPromedio() {
+        return promedio;
+    }
+
+    public String obtenerNombreEstudiante() {
+        return nombreEstudiante;
+    }
+
+    public double obtenerCalificacion1() {
+        return calificacion1;
+    }
+
+    public double obtenerCalificacion2() {
+        return calificacion2;
+    }
+
+    public double obteneraClificacion3() {
+        return calificacion3;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("Libretas de Calificacones\n"
+                + "Nombre del Estudiante : %s\n"
+                + "Calificacion 1 : %.3f\n"
+                + "Calificacion 2 : %.3f\n"
+                + "Calificacion 3 : %.3f\n"
+                + "Promedio : %.3f\n",
+                obtenerNombreEstudiante(),
+                obtenerCalificacion1(),
+                obtenerCalificacion2(),
+                obteneraClificacion3(),
+                obtenerPromedio());
+        return cadena;
+
+    }
+
+}
---
+++ problema1/problema01/src/proyecto001/ejecutor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto001;
+
+import paquete02.LibretaNotas;
+
+/**
+ *
+ * @author utpl
+ */
+public class ejecutor {
+
+    public static void main(String[] args) {
+        String nombreEstudiante;
+        double calificacion1;
+        double calificacion2;
+        double calificacion3;
+        double promedio;
+        LibretaNotas libreta1 = new LibretaNotas("Erick Toledo", 10, 9, 8);
+        libreta1.establecerPromedio();
+        LibretaNotas libreta2 = new LibretaNotas("Erick Rodas", 7, 9);
+        libreta2.establecerPromedio();
+        
+        System.out.printf("%s\n"
+                + "%s\n", libreta1,libreta2);
+        
+    }
+}
